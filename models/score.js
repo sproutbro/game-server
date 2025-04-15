33ef9d76ab068e3cdc974b6f5a3ce16910f2d4fc
Jackie <krsproutbro@google.com>
2025-04-16 05:58:04 +0900

    runner 최고 기록만 저장

--- models/score.js
+++ models/score.js
@@ -12,3 +12,37 @@ export async function saveScore({ userId, game, score }) {
     );
     return result.rows[0];
 }
+
+export async function findScoreById(userId) {
+    const result = await db.query(
+        `SELECT 
+            id, score
+        FROM 
+            SCORES 
+        WHERE
+            USER_ID = $1
+        ORDER BY 
+            SCORE 
+        DESC LIMIT 1`,
+        [userId]
+    )
+
+    return result.rows[0] ?? { id: null, score: null };
+}
+
+export async function updateScore(id, score) {
+    const result = await db.query(
+        `UPDATE 
+            scores
+        SET 
+            score = $2, 
+            created_at = now()
+        WHERE 
+            id = $1
+        RETURNING 
+            score`,
+        [id, score]
+    )
+
+    return result.rows[0] ?? null;
+}
--- routes/scores.js
+++ routes/scores.js
@@ -1,25 +1,51 @@
 import express from 'express';
-import { saveScore } from "../models/score.js";
+import { saveScore, findScoreById, updateScore } from "../models/score.js";
 const router = express.Router();
 
 router.post("/scores", async (req, res) => {
-    if (!req.user) {
-        return res.status(201).json({ message: '비회원 완료' });
-    }
-
     const userId = req.user?.id;
-    const score = req.body.score;
+    const { id, score } = await tryFindScoreById(userId);
+    const newScore = req.body.score;
 
-    if (!userId || !score) {
+    if (!userId || !newScore) {
         return res.status(400).json({ message: '필수 값 누락' });
     }
 
-    try {
-        const id = await saveScore({ userId, game: "runner", score });
-        res.status(201).json({ message: '점수 등록 완료', data: id });
-    } catch (error) {
-        console.error(error);
+    if (!id) {
+        const id = await trySaveScore(userId, newScore);
+        if (id) return res.status(201).json({ message: 'Save new score', score: newScore });
     }
+
+    if (newScore < score) {
+        return res.status(201).json({ message: 'Best score', score });
+    }
+
+    const bestScore = await tryUpdateScore(id, newScore);
+    if (bestScore) res.status(201).json({ message: 'New best score', score: newScore });
 });
 
 export default router;
+
+async function tryFindScoreById(userId) {
+    try {
+        return await findScoreById(userId);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function trySaveScore(userId, score) {
+    try {
+        return await saveScore({ userId, game: "runner", score });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function tryUpdateScore(id, newScore) {
+    try {
+        return await updateScore(id, newScore);
+    } catch (error) {
+        console.error(error);
+    }
+}
